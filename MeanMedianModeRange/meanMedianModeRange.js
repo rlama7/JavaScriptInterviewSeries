227fda8ab725184ada635d2620a1b42dcacc294e
Ratna Lama <rlama@ratnalamaCOM.local>
2021-01-26 19:25:44 -0800

    added Helper array sort function

--- MeanMedianModeRange/meanMedianModeRange.js
+++ MeanMedianModeRange/meanMedianModeRange.js
@@ -4,7 +4,12 @@
  * Arithmetic Mean also known as mean or average is obtained by:
  * mean = (1/n) Σ x_i         ( with i = 1 to n ) ... So: x1/n + x2/n + ... + xn/n
  * 
- * Median
+ * Arithmentic Median is a positional average and refers tot he middle value in a distribution
+ * sort the number
+ * if odd number, divide by 2 and round up to ge the position of the median number
+ * if even number, divide by 2. Go to the number in that position and average it with 
+ * the number in the next higher position to get the median.
+ * 
  * Mode or frequency
  * Range
  * 
@@ -52,12 +57,17 @@ const findSum = (...arr) => {
 const findMin = (arr) => Math.min(...arr);
 
 // calculate Max
+const findMax = (arr) => Math.max(...arr);
 
 // Helper function to round number to five digits precision
 const roundToFiveDigit = (num) => (Math.round(num *10000) + Number.EPSILON) / 10000;
 // console.log("round to five --> " + roundToFiveDigit(3.7142857142857144));
 
-const arr1 = [3,5,9,0,-6,10,5];
+// Helper function to sort number 
+const sortArrayElements = (...arr) => arr.sort((a, b) => a - b);
+
+
+const arr1 = [3,5,9,13,-6,10,5,3];
 const arr2 = [10,20,50];
 // console.log('sum --> ' + Math.sum(...arr2));
 
@@ -70,6 +80,8 @@ console.log('\n---------------Original Array1---------------------');
 console.log(arr1);
 console.log('\n---------------Destructured Original Array1 Elements---------------------');
 console.log(...arr1);
+console.log('\n---------------Destructred and Sorted Array1 Elements---------------------');
+console.log(arr1.sort());
 console.log('\n---------------findSum()---------------------');
 let sum = findSum(...arr1);
 console.log(`Sum of array1 elements --> ${sum}`);
@@ -80,11 +92,16 @@ console.log('Mean of array1 elements after rounding --> ' + findMean(arr1));
 
 console.log('\n---------------findMedian()---------------------');
 
-console.log('\n---------------findMax()---------------------');
-console.log('Max --> ' + Math.max(...arr1));
-
 console.log('\n---------------findMin()---------------------');
-console.log('Minima in Array1 --> ' + findMin(arr1));
+console.log('Minimum value in Array1 --> ' + findMin(arr1));
+
+console.log('\n---------------findMax()---------------------');
+console.log('Maximum value in Array1 --> ' + findMax(arr1));
+
+console.log('\n---------------sortArrayElements()---------------------');
+console.log('Sort elements in Array1 --> ' + sortArrayElements(...arr1));
+
+
 
 
 
