625cc43e529f530537293a13cb8d54b40790cf34
Ratna Lama <rlama@ratnalamaCOM.local>
2021-01-26 18:08:12 -0800

    updated findSum function

--- MeanMedianModeRange/meanMedianModeRange.js
+++ MeanMedianModeRange/meanMedianModeRange.js
@@ -1,7 +1,9 @@
 /**
  * Give an array compute mean and mode.
  * 
- * Arithmetic Mean also known as mean or average 
+ * Arithmetic Mean also known as mean or average is obtained by:
+ * mean = (1/n) Σ x_i         ( with i = 1 to n ) ... So: x1/n + x2/n + ... + xn/n
+ * 
  * Median
  * Mode or frequency
  * Range
@@ -15,18 +17,15 @@
  * [6,9,6,2,1,4,2] --> Mean=4.2857, Mode=2 (although both both 2 and 6 has the same frequency=2 we pick the mode of the smallest number)
  */
 
-const findMeanMedianModeRange = () => {
+const findMeanMedianModeRange = (arr) => {
     const result = [];
-    result.push(findMean());
-    result.push(findMedian());
-    result.push(findMode());
-    result.push(findRange());
+    result.push(findMean(arr), findMedian(arr), findMode(arr), findRange(arr), findSum(arr));
     return result;
 };
 
 // calculate Mean
 const findMean = (arr) => {
-    return 1;
+    return findSum() / arr.length;
 };
 
 // calculate Median
@@ -44,8 +43,32 @@ const findRange = (arr) => {
     return 7;
 };
 
+// calculate the sum of all values in a given aray
+const findSum = (...arr) => {
+    return arr.reduce((accumulatorValue, currentValue) => accumulatorValue+currentValue, 0);
+};
+
+// calculate Min
+
+// calculate Max
 
 
 
-const arr = [3,5,9,0,-6,10,5];
-console.log(findMeanMedianModeRange());
+const arr1 = [3,5,9,0,-6,10,5];
+const arr2 = [10,20,50];
+// console.log('sum --> ' + Math.sum(...arr2));
+console.log('Max --> ' + Math.max(...arr2));
+console.log('Min --> ' + Math.min(...arr2));
+arr1.push(...arr2);
+console.log(arr1);
+
+console.log('/n---------------findSum---------------------');
+console.log('Sum of array1 elements -->' + findSum(arr1));
+
+console.log('/n---------------findMean---------------------');
+console.log('Mean of array1 elements --> ' + findMean(arr1));
+
+console.log('/n---------------findMedian---------------------');
+console.log('');
+console.log(findMeanMedianModeRange(arr2));
+console.log(arr2.reduce((a,b) => a+b, 0));
