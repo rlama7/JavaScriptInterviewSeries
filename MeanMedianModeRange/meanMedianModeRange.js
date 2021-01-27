1db362ae5a93dfd383a044ce330b2e0b0ce7f8d1
Ratna Lama <rlama@ratnalamaCOM.local>
2021-01-27 13:56:40 -0800

    refactored calculation for finding mean median mode range min and max in a given array elements

--- MeanMedianModeRange/meanMedianModeRange.js
+++ MeanMedianModeRange/meanMedianModeRange.js
@@ -77,7 +77,9 @@ const findMedian = (arr) => {
 const findMode = (arr) => {
     // create an object hash of array elements
     let count = {};
+    let max = 0;
 
+    // const arrT = [1,2,3]
     arr.forEach((item) => {
         if (count[item] === undefined) {
             count[item] = 0;
@@ -88,18 +90,26 @@ const findMode = (arr) => {
     // convert object to array
     // Arr1 --> count: {"1":4,"3":2,"5":2,"7":1,"10":1,"11":1,"-2":1}
     // Arr2 --> count: {"1":1,"3":1,"5":1,"6":1,"9":1}
+    console.log('count object --> ' + JSON.stringify(count));
 
-    // 
-    let convertedArr = Object.values(count);
+    // Fetch only values from object
+    let fetchValuesFromObj = Object.values(count);
+    console.log('fetchValuesFromObj --> ' +  fetchValuesFromObj);
+
     // use Max/Min to find the max and min from the array
-    return count;
+    // Math.max(...fetchValuesFromObj)
+    // convert string to int
+    return parseInt(getKeyByValue(count, findMax(fetchValuesFromObj)));
 };
 
 // calculate Range
 const findRange = (arr) => {
-    const sortedArr = sortArrayElements(...arr);
-    let len = sortedArr.length;
-    return (sortedArr[len - 1] - sortedArr[0]);
+    // const sortedArr = sortArrayElements(...arr);
+    // let len = sortedArr.length;
+    // return (sortedArr[len - 1] - sortedArr[0]);
+
+    // alternatively faster approach
+    return findMax(arr) - findMin(arr);
 };
 
 // calculate the sum of all values in a given aray
@@ -115,64 +125,84 @@ const findMax = (arr) => Math.max(...arr);
 
 // Helper function to round number to five digits precision
 const roundToFiveDigit = (num) => (Math.round(num *10000) + Number.EPSILON) / 10000;
-// console.log("round to five --> " + roundToFiveDigit(3.7142857142857144));
 
 // Helper function to sort number 
 const sortArrayElements = (...arr) => arr.sort((a, b) => a - b);
 
+// Helper function to Get Key By Value from Object
+const getKeyByValue = (obj, val) => {
+    return Object.keys(obj).find(key => obj[key] === val);
+};
+
+// Helper function to find even or odd  LENGTH
+const isEven = (arr) => {
+    const len = arr.length;
+    return arr.length % 2 === 0 ? `EVEN --> (${len})` : `ODD --> (${len})`;
+};
+
 
 const arr1 = [1,3,5,1,7,-2,10,5,3,1,11,1];
-const arr2 = [1, 6, 9, 5, 3];
-// console.log('sum --> ' + Math.sum(...arr2));
-
-// arr1.push(...arr2);
-// console.log(arr1);
-
-
+console.log(` arr1 ${arr1}`);
+const arr2 = [3, 6, 9, 5, 3, 1, 1];
+const arr3 = [1,2,3,4,5,6];
+const arr4 = [3,4,5,3,4,5,4,5,4,5,5];
 
 console.log('\n---------------Original Array1---------------------');
 console.log(arr1);
+
 console.log('\n---------------Destructured Original Array1 Elements---------------------');
 console.log(...arr1);
-console.log('\n---------------Destructred and Sorted Array1 Elements---------------------');
+
+console.log('\n---------------Sorted Array1 Elements---------------------');
 console.log(sortArrayElements(...arr1));
+
 console.log('\n---------------findSum()---------------------');
 let sum = findSum(...arr1);
 console.log(`Sum of array1 elements --> ${sum}`);
 
 console.log('\n---------------findMean()---------------------');
 console.log('Mean of array1 elements before rounding --> ' + (sum)/arr1.length);
-console.log('Mean of array1 elements after rounding --> ' + findMean(arr1));
+console.log('Mean of array1 elements after rounding to FIVE digits --> ' + findMean(arr1));
 
 console.log('\n---------------findMedian()---------------------');
+console.log('original arra1 --> ' + arr1);
 console.log('sorted array1 --> ' + sortArrayElements(...arr1));
-console.log('median of array1 elements EVEN LENGTH--> ' + findMedian(arr1));
+console.log(`Median of array1 elements ${isEven(arr1)} Length  -->  + ${findMedian(arr1)}\n`);
+
+console.log('original arra2 --> ' + arr2);
 console.log('sorted array2 --> ' + sortArrayElements(...arr2));
-console.log('median of array2 elements ODD LENGTH--> ' + findMedian(arr2));
+console.log(`Median of array2 elements ${isEven(arr2)} LENGTH-->  + ${findMedian(arr2)}\n`);
+
+console.log('original arra3 --> ' + arr3);
+console.log('sorted array3 --> ' + sortArrayElements(...arr3));
+console.log(`median of array3 elements ${isEven(arr3)} LENGTH-->  + ${findMedian(arr3)}\n`);
+
+console.log('original arra4 --> ' + arr4);
+console.log('sorted array4 --> ' + sortArrayElements(...arr4));
+console.log(`median of array4 elements ${isEven(arr4)} LENGTH-->  + ${findMedian(arr4)}\n`);
 
 console.log('\n---------------findMode()---------------------');
-console.log('Mode Object in Array1 --> ' + JSON.stringify(findMode(arr1)));
-console.log('Mode Object in Array2 --> ' + JSON.stringify(findMode(arr2)));
+console.log('\nMode in Array1 --> ' + findMode(arr1));
+console.log('\nMode in Array2 --> ' + findMode(arr2));
+console.log('\nMode in Array3 --> ' + findMode(arr3));
+console.log('\nMode in Array4 --> ' + findMode(arr4));
 
 console.log('\n---------------findRange()---------------------');
-console.log('Range in Array1 --> ' + findRange(arr1));
-console.log('Range in Array2 --> ' + findRange(arr2));
-
+console.log(`Range in Array1: [${arr1}] Max: ${findMax(arr1)} Min: ${findMin(arr1)} -->  + ${findRange(arr1)}`);
+console.log(`Range in Array2: [${arr2}] Max: ${findMax(arr2)} Min: ${findMin(arr2)} -->  + ${findRange(arr2)}`);
+console.log(`Range in Array3: [${arr3}] Max: ${findMax(arr3)} Min: ${findMin(arr3)} -->  + ${findRange(arr3)}`);
+console.log(`Range in Array4: [${arr4}] Max: ${findMax(arr4)} Min: ${findMin(arr4)} -->  + ${findRange(arr4)}`);
 
 console.log('\n---------------findMin()---------------------');
-console.log('Minimum value in Array1 --> ' + findMin(arr1));
+console.log(`findMin in Array1: [${arr1}]  Min: --> ${findMin(arr1)}`);
 
 console.log('\n---------------findMax()---------------------');
-console.log('Maximum value in Array1 --> ' + findMax(arr1));
+console.log(`findMax in Array1: [${arr1}]  Max: --> ${findMax(arr1)}`);
 
 console.log('\n---------------sortArrayElements()---------------------');
-console.log('Sort elements in Array1 --> ' + sortArrayElements(...arr1));
+console.log(`Sort elements in Array1: [${arr1}] -->  + ${sortArrayElements(...arr1)}`);
 
-console.log('len of arr1 --> ' + arr1.length);
-console.log('len/2 of arr1 --> ' + arr1.length/2);
+console.log('\n---------------roundToFiveDigit()---------------------');
+console.log("round to five digit: 3.7142857142857144 --> " + roundToFiveDigit(3.7142857142857144));
 
-
-
-// console.log('');
-// console.log(findMeanMedianModeRange(arr2));
-// console.log(arr2.reduce((a,b) => a+b, 0));
+console.log('\n------------------------------------------------------\n');
