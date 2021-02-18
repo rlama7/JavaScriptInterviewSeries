c19a3956b3bb2a205c107258f71af875a4cecdd8
Ratna Lama <rlama@ratnalamaCOM.local>
2021-02-17 18:23:07 -0800

    added examples for a tree traversal

--- Tree/treeTraversal.js
+++ Tree/treeTraversal.js
@@ -4,6 +4,22 @@
  * @since                - 1.0.0
  * @description          - Traverse through the tree in a depth first / breadth first search fashion
  *
+ *
+ * tree =             1
+ *                /       \
+ *               2         3
+ *              /  \      /  \
+ *             4   5     6   7
+ *
+ *             |   |     |    |
+ *            [ .  leaf nodes ]
+ *
+ *
+ * depthFirstSearch = [1,2,4,5,3,6,7]
+ *
+ * breadthFirstSearch = [1, 2, 3, 4,5,6,7]
+ *
+ *
  * Time Complexity: O(n) = linear
  * Space Complexity: O(n) = linear
  */
