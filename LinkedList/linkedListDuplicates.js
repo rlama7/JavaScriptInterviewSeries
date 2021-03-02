fb0d61dddd91d13b70647109c216cf88b0a70f4b
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-02 10:48:30 -0800

    added documentation

---
+++ LinkedList/linkedListDuplicates.js
@@ -0,0 +1,17 @@
+/**
+ * linkedLIstDuplicates.js      - A program to remove duplicate values from a given linkedlist.
+ * @author                      - Ratna Lama
+ * @version                     - 1.0.0
+ * @description
+ *
+ * Example -
+ *
+ * linkedlist: 1 -> 2 -> 2 -> 3 -> 3 -> 4 -> 5 -> 5 -> 5-> 5
+ *             |
+ *          Head Node
+ *
+ * expected: 1 -> 2 -> 3 -> 4 -> 5
+ *
+ * Algorithm:
+ *
+ */
