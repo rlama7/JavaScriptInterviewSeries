8b7efe5a0301f8dd3512a9203f19acee74960ac7
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-02 12:55:38 -0800

    added remove duplicates from the singly linked list

--- LinkedList/linkedListDuplicates.js
+++ LinkedList/linkedListDuplicates.js
@@ -2,7 +2,9 @@
  * linkedLIstDuplicates.js      - A program to remove duplicate values from a given linkedlist.
  * @author                      - Ratna Lama
  * @version                     - 1.0.0
- * @description
+ * @description                 - LinkedList nodes are sorted in order. The modified LinkedList
+ *                                should be in place, i.e., don't create a brand new list. And the
+ *                                the modified list should have nodes sorted in order.
  *
  * Example -
  *
@@ -15,3 +17,23 @@
  * Algorithm:
  *
  */
+
+class LinkedList {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function removeLinkedListDuplicates(linkedList) {
+  let currentNode = linkedList;
+  while (currentNode !== null && currentNode.next !== null) {
+    // check for duplicate data
+    if (currentNode.data === currentNode.next.data) {
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+  return linkedList;
+}
