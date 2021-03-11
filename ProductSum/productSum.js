eda0f784d7a67b00359f0ad504c3f3b07d379e2a
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-11 14:07:28 -0800

    initiated productSum file

---
+++ ProductSum/productSum.js
@@ -0,0 +1,11 @@
+/**
+ * productSum.js    - A program to calculate a function that takes in a special arrya and returns its product sum.
+ * @author          - Ratna Lama
+ * @version         - 1.0.0
+ * @description
+ *
+ * Example -
+ *
+ *
+ */
+const productSum = (array) => {};
