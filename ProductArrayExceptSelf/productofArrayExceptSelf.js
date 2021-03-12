410f93dbfe119390c5e4ff2b67730bf615a3404d
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-11 17:49:16 -0800

    added initial description

---
+++ ProductArrayExceptSelf/productofArrayExceptSelf.js
@@ -0,0 +1,18 @@
+/**
+ * productofArrayExceptSelf.js      - A program to calculate product of an array except self.
+ * @author                          - Ratna Lama
+ * @version                         - 1.0.0
+ * @description                     - Given an array (arr), return an output (arrProduct) such that the
+ *                                    output is equal to the product of all the elements of array (arr)
+ *                                    except arr[i].
+ *
+ * Example -
+ * Input:   [5, 2,  3,  4]
+ * Output: [24, 60, 40, 30]
+ *
+ * Time Complexity: O(n) --> Linear
+ * Space Complexity: O(n) --> Linear
+ */
+const productofArrayExceptSelf = (arr) => {};
+
+module.exports = productofArrayExceptSelf;
