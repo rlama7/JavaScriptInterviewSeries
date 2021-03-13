79e7b2a8277102df2be66ec5aacc4b50ae522da8
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-13 14:53:35 -0800

    initiated three largest number problem

---
+++ ThreeLargestNumbers/threeLargestNumbers.js
@@ -0,0 +1,20 @@
+/**
+ * threeLargestNumbers.js   - A program to find the three largest number from a given array of at least three elements.
+ * @author                  - Ratna Lama
+ * @version                 - 1.0.0
+ * @description             - The array will have at least three elements.
+ *                            Find the first three largest numbers and return them.
+ *                            If the duplicates exists then return the duplicates.
+ *                            DO NOT SORT the array to solve the challenge.
+ *
+ * Example -
+ * INPUT:
+ *       array: [8, 5, 7, 1, 15, 3, 8]
+ * OUTPUT: [8, 15, 8]
+ *
+ * Algorithm:
+ *
+ * Time Complexity: O(n) -->
+ * Space Complexity: O(n) -->
+ */
+const threeLargestNumbers = (arr) => {};
