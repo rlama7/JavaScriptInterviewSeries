f753475efae03dc187f61142e0f3e5f011313f20
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-20 12:36:02 -0700

    initiated intial documents for the problem

---
+++ SortedSquareArray/sortedSquareArray.js
@@ -0,0 +1,17 @@
+/**
+ * sortedSquareArray.js     - A program to find a sorted square array given a sorted array.
+ * @author                  - Ratna Lama
+ * @version                 - 1.0.0
+ * @description             - Input array is sorted. Output squared array must also be sorted.
+ *                          - Restrictions: Attempt for the linear time complexity, i.e. O(n)
+ *
+ * Example --
+ *
+ * Algorithm --
+ *
+ *
+ * Time Complexity: O(n) --> linear
+ * Space Complexity: O(n) --> linear
+ *
+ */
+const sortedSquaredArray = (array) => {};
