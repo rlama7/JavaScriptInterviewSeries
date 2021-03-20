e6373629c6ca323c1c32f8633a494567e766528b
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-20 12:25:58 -0700

    added name, salary and age sort functionality

--- ObjectOperation/person.js
+++ ObjectOperation/person.js
@@ -64,6 +64,9 @@ const person = [
   },
 ];
 
+console.log(
+  '\n----------------BEGIN---------------------------------------------'
+);
 /**
  * Average Salary for the persons
  *   */
@@ -73,7 +76,7 @@ const salaryArray = [];
 person.map((item, index) => {
   salaryArray.push(parseInt(person[index].salary));
 });
-
+console.log('salaryArray: ');
 console.log(salaryArray);
 
 // sum salary array
@@ -83,6 +86,7 @@ console.log('salary sum -> ' + sum);
 
 const averageSalary = sum / salaryArray.length;
 console.log(`average Salary --> ${averageSalary}`);
+
 console.log('\n-------------------------------------------------------------');
 
 /**
@@ -105,18 +109,56 @@ const getAge = (dob) => {
   return age;
 };
 
-const name = person.filter((dob, index) => {
+const fullName = person.filter((dob, index) => {
   if (getAge(person[index].DOB) > 32)
     return person[index].firstName + person[index].lastName;
 });
-
-console.log(name);
-console.log('\n-------------------------------------------------------------');
-// Sorted on the basis of Age (Youngest to Oldest)
-
+console.log('sorted person that are age > 32: ');
+console.log(fullName);
 console.log('\n-------------------------------------------------------------');
 
+/**
+ * Sorted on the basis of Age (Youngest to Oldest)
+ *
+ **/
+const sortedPersonOnAge = person.sort((a, b) => getAge(b.DOB) - getAge(a.DOB));
+console.log(
+  'sorted person based on DOB YEAR and not Month and DAY: --> need updates here! '
+);
+console.log(sortedPersonOnAge);
+
+/**
+ * Sorted on the basis of Salary (ascending order)
+ *
+ **/
 console.log('\n-------------------------------------------------------------');
+const sortedPersonOnSalary = person.sort(
+  (a, b) => parseInt(a.salary) - parseInt(b.salary)
+);
+
+console.log('sorted person on salary: ');
+console.log(sortedPersonOnSalary);
+console.log('\n-------------------------------------------------------------');
+
+/**
+ * Sorted on the basis of lastName (a to z) ignore upper and lowercase
+ *
+ **/
+const sortPersonOnLastName = person.sort(
+  (a, b) => a.lastName.toLowerCase() - b.lastName.toLowerCase()
+);
+
+console.log('sort person based on Last name: ');
+console.log(sortPersonOnLastName);
+
+console.log('\n-------------------------------------------------------------');
+const sortPersonOnFirstName = person.sort(
+  (a, b) => a.firstName.toLowerCase() - b.firstName.toLowerCase()
+);
+console.log('sort person based on First name: ');
+console.log(sortPersonOnFirstName);
+console.log('\n-------------------------------------------------------------');
+
 /**
  * Utility functions
  *
@@ -140,7 +182,7 @@ console.log(odd);
 // use set --> can only contain unique elements
 // #1
 const uniqueArr = [...new Set(arr)];
-console.log('uniqueArr:');
+console.log('uniqueArr using Set:');
 console.log(uniqueArr);
 
 // remove duplicates using filter
@@ -194,4 +236,6 @@ let diff =
     : 'younger';
 console.log(`Who's older? ${diff}`);
 
-console.log('\n-------------------------------------------------------------');
+console.log(
+  '\n-------------------END------------------------------------------'
+);
