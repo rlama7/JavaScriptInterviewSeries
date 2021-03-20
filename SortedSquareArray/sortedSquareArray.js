c1813ae23415de6c0e8ea6607740ca0e10a54f6e
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-20 12:47:35 -0700

    added first attempt with nlogn time complexity

--- SortedSquareArray/sortedSquareArray.js
+++ SortedSquareArray/sortedSquareArray.js
@@ -6,6 +6,10 @@
  *                          - Restrictions: Attempt for the linear time complexity, i.e. O(n)
  *
  * Example --
+ *      INPUT:
+ *             array = [-7, -5, -2, -1,  3, 4, 6]
+ *      OUTPUT:
+ *       sortedArray = [1, 4, 9, 16, 25, 36, 49]
  *
  * Algorithm --
  *
@@ -14,4 +18,27 @@
  * Space Complexity: O(n) --> linear
  *
  */
-const sortedSquaredArray = (array) => {};
+
+/**
+ *
+ * @param {*} array sorted input array
+ *
+ * Time complexity - O(n log n);
+ * Space Complexity - O(n) - linear
+ *
+ * COMMENT - NOT OPTIMAL!
+ */
+const sortedSquaredArray = (array) => {
+  // square the array
+  const squaredArray = array.map((x) => x * x);
+
+  // sort the array
+  return squaredArray.sort((a, b) => a - b);
+};
+
+const array = [-7, -5, -2, -1, 3, 4, 6];
+console.log('First attempt: ');
+console.log(sortedSquaredArray(array)); // [1,  4,  9, 16, 25, 36, 49]
+console.log('\n--------------------------------------------------------');
+
+const sortedSquareArrayOptimal = (array) => {};
