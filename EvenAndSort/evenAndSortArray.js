38b54c6754afce98639a5ba971ff64790b85eac4
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-23 11:57:18 -0700

    added sort even and odd subarray functionality

--- EvenAndSort/evenAndSortArray.js
+++ EvenAndSort/evenAndSortArray.js
@@ -5,9 +5,38 @@
  * @description         -
  *
  * Example -
+ *      INPUT:
+ *          array = [12,3,2,1,7,6]
+ *      OUTPUT:
+ *          evenAndSortedArray = [2,6,12]
+ *          oddAndSortedArray = [ 1, 3, 7 ]
+ *
  *
  * Time Complexity -
- *          Time Complexity: O(n) -->
- *          Space Complexity: O() -->
+ *          Time Complexity: O(n log n) --> since the best sort algorightm runs in O(n log n)
+ *          Space Complexity: O(1) --> In place, no extra space consumed.
  */
-const evenAndSortArray = (arr) => {};
+const evenAndSortedArray = (arr) => {
+  console.log(arr);
+  // sort
+  arr.sort((a, b) => a - b);
+  console.log(arr);
+  // filter out even
+  return arr.filter((item) => item % 2 === 0);
+};
+
+const oddAndSortedArray = (arr) => {
+  console.log(arr);
+  // sort
+  arr.sort((a, b) => a - b);
+  console.log(arr);
+  // filter out odd
+  return arr.filter((item) => item % 2 !== 0);
+};
+const array = [12, 3, 2, 1, 7, 6];
+
+const arrEvenSorted = evenAndSortedArray(array);
+console.log(arrEvenSorted); // [ 2, 6, 12 ]
+
+const arrOddSorted = oddAndSortedArray(array);
+console.log(arrOddSorted); // [ 1, 3, 7 ]
