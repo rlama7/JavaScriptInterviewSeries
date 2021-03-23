cce878fa545a114382952d37e8ba58c938e85044
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-23 10:49:31 -0700

    added initial file descriptions for finding even sorted sub array from a given array

---
+++ EvenAndSort/evenAndSortArray.js
@@ -0,0 +1,13 @@
+/**
+ * evenAndSortArray.js  - A program to sort only even from a given array.
+ * @author              - Ratna Lama
+ * @version             - 1.0.0
+ * @description         -
+ *
+ * Example -
+ *
+ * Time Complexity -
+ *          Time Complexity: O(n) -->
+ *          Space Complexity: O() -->
+ */
+const evenAndSortArray = (arr) => {};
