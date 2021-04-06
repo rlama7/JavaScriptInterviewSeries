21fd2b019985f0e9b1a99f6acd5bad6bd234a0e4
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-06 13:35:39 -0700

    added initial description for finding monotonic array

---
+++ Array/monotonicArray.js
@@ -0,0 +1,20 @@
+/**
+ * monotonicArray.js  - A program to find if an array of integers is a monotonic.
+ * @author            - Ratna Lama
+ * @version           - 1.0.0
+ * @description       - A monotonic array elements are entirely non-increasing or
+ *                      entirely non-decreasing from left to right.
+ *                      An empty array and arrays of one element are monotonic
+ *
+ * Example -
+ *
+ * Algorithm -
+ *      INPUT:
+ *      OUTPUT:
+ *
+ * Time Complexity: O() -->
+ * Space Complexity: O() -->
+ */
+const monotonicArray = (array) => {
+  return true;
+};
