5cdff15c4916d724f70c198b12e44db8f14a98b8
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-09 13:27:03 -0700

    initiated spiral traverse documenation

---
+++ Array/spiralTraverse.js
@@ -0,0 +1,26 @@
+/**
+ * spiralTraverse.js    - A program to find the spiral array from a n x m two-dimensional array.
+ * @author              - Ratna Lama
+ * @version             - 1.0.0
+ * @description         - Spiral order starts from the tope left corner fo the two-d array,
+ *                        goes to the right, and proceeds in a spiral pattern all the way
+ *                        until every element has been visited.
+ *
+ * Example -
+ *      INPUT:
+ *          array = [
+ *                      [10, 20, 30],
+ *                      [40, 50, 60],
+ *                      [70, 80, 90],
+ *                  ]
+ *      OUTPUT:
+ *          array = [10, 20, 30, 60, 90, 80, 70, 40, 50]
+ *
+ * Algorithm -
+ *
+ *
+ * Time Complexity: O() -->
+ * Space Complexity: O() -->
+ *
+ */
+const spiralTraverse = (array) => {};
