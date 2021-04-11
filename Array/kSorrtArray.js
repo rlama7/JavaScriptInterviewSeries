37a763536b40e57c10282fe1d2d8267d75cd06c3
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-11 12:27:43 -0700

    initiated total occurrence of k in a sorted array solution

---
+++ Array/kSorrtArray.js
@@ -0,0 +1,18 @@
+/**
+ * kSortArray.js    - A program to find total occurences of K in a sorted array.
+ * @author          - Ratna Lama
+ * @version         - 1.0.0
+ * @description     -
+ *
+ * Example -
+ *      INPUT:
+ *          array = []
+ *      OUTPUT:
+ *
+ * Algorithm -
+ *
+ * Time Complexity: O(log n) --> log of n
+ * Space Complexity: O(1) --> constant
+ *
+ */
+const kSortArray = (array, target) => {};
