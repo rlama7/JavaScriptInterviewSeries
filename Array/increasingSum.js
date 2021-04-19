0c465fe24677e4fb262ff97b19711a97d92ebd1c
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-18 18:22:30 -0700

    initiated increasing sum solution

---
+++ Array/increasingSum.js
@@ -0,0 +1,23 @@
+/**
+ * increasingSum.js     - A program to find the sum of increasing array elements.
+ * @author              - Ratna Lama
+ * @version             - 1.0.0
+ * @description         -
+ *
+ *
+ * Example -
+ *      INPUT
+ *          array = [1,2,3,4,5,6,7,8,9]
+ *      OUTPUT
+ *          restul = [1, 5, 15, 24]
+ *              1 <- 1
+ *              5 <- 2+3
+ *              15 <- 4+5+6
+ *              24 <- 7+8+9
+ *
+ * Algorithm -
+ *
+ * Time Complexity -
+ * Space Comlexity -
+ */
+const increasingSum = (array) => {};
