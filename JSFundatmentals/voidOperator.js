697f465c82c7c61cd05ca6b81e491115a65b6349
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-27 17:34:38 -0700

    added more usage

--- JSFundatmentals/voidOperator.js
+++ JSFundatmentals/voidOperator.js
@@ -7,7 +7,7 @@ testFunction();
 // This function will evalute the given expression and then return undefined
 // You won't be able to call it again!
 void (function anotherFunction() {
-  console.log('You can not see this print');
+  console.log('You can see this print only ONCE!');
 })();
 
 // anotherFunction(); <-- This will result in reference error as there is
@@ -17,3 +17,18 @@ void (function anotherFunction() {
 (function testX() {
   console.log('This is test function');
 })();
+
+/**
+ * In the following exmaple we combine javascript:void(0) to 
+ * tell the HTML element to run javascript  but do not navigate away 
+ * from the current page which is where Link is displayed currently.
+ * Thus void(0) in such case refere to do nothing- don't reload, don't navigate
+ */
+<a href="javascript:void(0)">Link</a>
+
+<a
+  id="link"
+  href="javascript:void(document.querySelector('#link').style.color='red')"
+>
+  Link
+</a>;
