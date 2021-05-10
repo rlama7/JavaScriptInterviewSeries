514e831b6f501f8efc5ef4b2746ee8e19a6ca461
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-10 11:56:59 -0700

    added first duplicate documenation

---
+++ Array/firstDuplicate.js
@@ -0,0 +1,26 @@
+/**
+ * firstDuplicate.js    - A program to find the first duplicate in a given array.
+ * @author              - Ratna Lama
+ * @version             - 1.0.0
+ * @param {*} array     - input array of integers
+ * @description         - Constraints/Restrictions:
+ *                        Don't mutate the input aray
+ *                        If no duplicates then return -1;
+ *
+ * --->Example<---
+ *      INPUT:
+ *         array1 = [12,3,4,5,2,3,4]
+ *         array2 = [9,3,4,5,5,4,3,9]
+ *      OUTPUT:
+ *          result1 = 3 since 3 repeats first before 4
+ *          result2 = 5 since 5 repeats firs before 4,3 or 9
+ *
+ * --->Algorithm<---
+ *
+ * --->Time Complexity<---
+ * Time O() ->
+ * Space O() ->
+ *
+ *
+ */
+const firstDuplicate = (array) => {};
