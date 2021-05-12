1c772b9cbdbd13664db4e17f624010a7b7498e07
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-12 11:38:30 -0700

    added iterative approach to the greatest divisor

--- Array/greatestCommonDivisor.js
+++ Array/greatestCommonDivisor.js
@@ -4,6 +4,10 @@
  * @version              - 1.0.0
  * @description          - CONSTRAINTS/RESTRICTIONS
  *                       - EDGE CASES
+ *                          - check if given inputs are numbers and otherwise return false
+ *                          - check if the given inputs are positive otherwise either return
+ *                            false or get the positive valuess
+ *
  * @param {*} a input number
  * @param {*} b input number
  *
@@ -19,4 +23,44 @@
  *
  * --->Time Complexity<---
  */
-const greatestCommonDivisor = (a, b) => {};
+const greatestCommonDivisor = (a, b) => {
+  // edge cases
+  // check if inputs are numbes otherwise return false or -1
+  if (typeof a !== 'number' || typeof b !== 'number') return false;
+
+  // check if number is negative
+  // if number is negative either we can return false or
+  // find the greatest common divisor of positive numbers
+  if (a < 0 || b < 0) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+  }
+  if (a === 0 && b === 0) return false;
+
+  while (b) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
+
+let a = 15,
+  b = 30;
+
+let x = '15',
+  y = '30';
+
+let s = -9,
+  t = -3;
+
+let w = 20,
+  v = 30;
+
+let p = 0,
+  q = 0;
+console.log(greatestCommonDivisor(a, b)); // 15
+console.log(greatestCommonDivisor(x, y)); // false
+console.log(greatestCommonDivisor(s, t)); // 3
+console.log(greatestCommonDivisor(w, v)); // 10
+console.log(greatestCommonDivisor(p, q)); // false
