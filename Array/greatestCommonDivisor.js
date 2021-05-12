de8dad6856a48c2c93a0ae136f8f8e0588291918
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-12 11:07:00 -0700

    added greatest common divisor initial documenattion

---
+++ Array/greatestCommonDivisor.js
@@ -0,0 +1,22 @@
+/**
+ * greatestCommonDivisor - A program to find the greatest common divisor (factor).
+ * @author               - Ratna Lama
+ * @version              - 1.0.0
+ * @description          - CONSTRAINTS/RESTRICTIONS
+ *                       - EDGE CASES
+ * @param {*} a input number
+ * @param {*} b input number
+ *
+ * --->Example<---
+ *      INPUT:
+ *          a: 15 -> 1, 3, 5, 15
+ *          b: 30 -> 1, 2, 3, 5, 6, 10, 15, 30
+ *      OUTPUT:
+ *          result: The factors that are common to both 15 and 30 are: 1, 3, 5, 15
+ *                  The greatest/largest common divisor/factor is 15
+ *
+ * --->Algorithm<---
+ *
+ * --->Time Complexity<---
+ */
+const greatestCommonDivisor = (a, b) => {};
