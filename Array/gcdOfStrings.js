3848e033094dbe2878c726003264bc4661dca85c
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-12 17:10:40 -0700

    initiated greatest common divisor of string in JavaScript

---
+++ Array/gcdOfStrings.js
@@ -0,0 +1,33 @@
+/**
+ * gcdOfStrings.js - A program to find the greatest common divisor of Strings.
+ * @author         - Ratna Lama
+ * @version        - 1.0.0
+ * @description    -
+ *
+ * --->Example<---
+ *
+ * #1
+ *  Input: str1 = 'ABCABC', str2 = 'ABC'
+ *  Output: 'ABC'
+ *
+ * #2
+ *  Input: str1 = 'ABABAB', str2 = 'ABAB'
+ *  Output: 'AB'
+ *
+ * #3
+ *  Input: str1 = 'HELLO', str2 = 'WORLD'
+ *  Output: ''
+ *
+ * #4
+ *  Input: str1 = 'ABCDEF', str2 = 'ABC'
+ *  Output: ''
+ *
+ *
+ *
+ * --->Algorithm<---
+ * --->Time Complexity<---
+ *
+ * @param {*} str1 input string
+ * @param {*} str2 input string
+ */
+const gcdOfStrings = (str1, str2) => {};
