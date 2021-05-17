af855ec83cfe860873092f41f1c6fcde40baee18
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-16 20:13:28 -0700

    added splice example

--- Array/moveToEnd/moveTargetToEndInPlaceInOrder.js
+++ Array/moveToEnd/moveTargetToEndInPlaceInOrder.js
@@ -75,4 +75,17 @@ console.log(moveTargetToEndInPlaceInOrder(array5, target5)); // [ 'b', 'd', 'e',
 /**
  * UTILITY
  * slice Vs splice
+ *
+ * for splice index begins at 0
  */
+
+const arr1 = [1, 2, 3, 4, 5, 6];
+const arr2 = ['a', 'b', 'c', 'd', 'e'];
+
+arr1.splice(1, 1); // remove 1 element at index 1
+arr2.splice(1, 1); // // remove 1 element at index 1
+console.log(arr1, arr2); // [ 1, 3, 4, 5, 6 ] [ 'a', 'c', 'd', 'e' ]
+
+arr1.splice(2, 0, 'January'); // at index 2 remove 0 element and add 'January'
+arr2.splice(2, 0, 100); // at index 2 remove 0 element and add 100
+console.log(arr1, arr2); // [ 1, 3, 'January', 4, 5, 6 ] [ 'a', 'c', 100, 'd', 'e' ]
