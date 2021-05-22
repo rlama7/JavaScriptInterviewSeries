eaa4d0cc5ae5fc5d67f45a0a2447b0331bb78d24
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-21 18:42:50 -0700

    added sub array sort initial commit

---
+++ Array/subArraySort.js
@@ -0,0 +1,7 @@
+/**
+ * subArraySort.js - A program to find the smallest subarray in the input
+ *                   array that needs to sorted in place in order for the
+ *                   entire input array to be sorted (in ascending order)
+ * @param {*} array input array to sorted in place
+ */
+const subArraySort = (array) => {};
