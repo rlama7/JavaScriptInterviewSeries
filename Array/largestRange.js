4336ee8b25b7a4c7ed366e1e5c1b0ff9dff40492
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-25 13:36:27 -0700

    initiated largest range solution

---
+++ Array/largestRange.js
@@ -0,0 +1,24 @@
+/**
+ * largestRange.js - A program to find the largest range of integers contained in the list.
+ * @author         - Ratna Lama
+ * @version        - 1.0.0
+ * @description    - A range of number is a set of consecutive numbers in the list. Note that numbers
+ *                   don't need to sorted or adjacent in the input array.
+ * @param {*} array input array
+ *
+ * --->Example<---
+ *      INPUT:
+ *          array = [1,5,3,2,12,22,4,10]
+ *      OUTPUT:
+ *          result = [1,5]
+ * --->Algorithm<---
+ *
+ * --->Time Complexity<---
+ * time O() ->
+ * space O() ->
+ */
+
+const largestRange = (array) => {};
+
+const array1 = [1, 5, 3, 2, 12, 22, 4, 10];
+console.log(largestRange(array1));
