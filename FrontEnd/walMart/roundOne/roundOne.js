5e47790d61b56c88be19434d80f83dbdf1591d0d
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-26 13:38:54 -0700

    added getLength with edge cases solution

---
+++ FrontEnd/walMart/roundOne/roundOne.js
@@ -0,0 +1,29 @@
+/**
+ * #1
+ * Create a function named getLength which takes a string as an argument
+ * and returns the length of the string:
+ *
+ * --->Example<---
+ * INPUT: getLength('foo')
+ * OUTPUT: 3
+ *
+ * INPUT: getLength('hello')
+ * OUTPUT: 5
+ */
+const getLength = (string) => {
+  // for undefined and null as an input return some message | -1;
+  if (string === undefined || string === null) return -1;
+
+  // if input is not a string then return message
+  if (typeof string !== 'string')
+    return 'Please enter a valid string as an input and Try again!';
+
+  return string.length;
+};
+
+console.log(getLength('')); // 0
+console.log(getLength(undefined)); // -1
+console.log(getLength(null)); // -1
+console.log(getLength(123456)); // Please enter a valid string as an input and Try again!
+console.log(getLength('foo')); // 3
+console.log(getLength('hello')); // 5
