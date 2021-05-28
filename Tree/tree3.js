bd8cd95738fed155b6c3a4e195a61c78bfe99db3
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-28 12:43:23 -0700

    initiated node class and tree class for tree traversal in JavaScripts

---
+++ Tree/tree3.js
@@ -0,0 +1,30 @@
+/**
+ * tree3.js     - A program to implement a node and a Tree class in JavaScripts.
+ * @author      - Ratna Lama
+ * @version     - 1.0.0
+ * @description - Implement a Node class and a Tree class.
+ *
+ */
+
+/**
+ * Create a node class. The constructor should accept an argumen that gets
+ * assigned to the data propeerty and initialize and empty array for storing
+ * children. The node class should have methods 'add' and 'remove'
+ */
+class Node {}
+
+/**
+ * Create a tree class. The tree constructor should initialize a 'root' property to null.
+ *
+ * Then on the tree class implement:
+ *  1) traverseBFS -> to traverse the tree in a level-order fashion or breadth first search.
+ *
+ *  2) traverseDFS -> to traverser the tree in a depth first search
+ *      NOTE: --> Depth First Search can be done in 3 ways:
+ *                1) preorder --> <root> <leftChild> <rightChild>
+ *                2) inorder  --> <leftChild> <root> <rightChild>
+ *                3) postorder --> <leftChild> <rightChild> <root>
+ *
+ *     NOTE: --> Inorder traversal of a Tree in a Depth First Search yields sorted list.
+ */
+class Tree {}
