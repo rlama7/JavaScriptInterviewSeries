8c7d765045b6c50577cd76ecd7129855b80f452a
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-31 17:46:37 -0700

    created nodes and connected them to form a graph

--- Graph/depthFirstSearch.js
+++ Graph/depthFirstSearch.js
@@ -43,3 +43,49 @@ class Graph {
     this.nodes.push(node);
   }
 }
+
+// create nodes
+const DFW = new Node('DFW');
+const LAX = new Node('LAX');
+const HNL = new Node('HNL');
+const SFO = new Node('SFO');
+const HKG = new Node('HKG');
+const EWR = new Node('EWR');
+const JFK = new Node('JFK');
+const BOS = new Node('BOS');
+const MIA = new Node('MIA');
+const MCO = new Node('MCO');
+const PBI = new Node('PBI');
+
+// create graphs
+const graph = new Graph([DFW, LAX, SFO, HKG, EWR, JFK, BOS, MIA, MCO, PBI]);
+
+// connect grpahs
+DFW.connect(LAX);
+LAX.connect(HNL);
+LAX.connect(EWR);
+LAX.connect(SFO);
+SFO.connect(HKG);
+DFW.connect(JFK);
+JFK.connect(BOS);
+JFK.connect(MIA);
+MIA.connect(MCO);
+MCO.connect(PBI);
+
+console.log(graph.nodes[0].edgesList[0]);
+/**
+ Graph {
+  nodes: [
+    Node { data: 'DFW', edgesList: [Array] },
+    Node { data: 'LAX', edgesList: [Array] },
+    Node { data: 'SFO', edgesList: [Array] },
+    Node { data: 'HKG', edgesList: [Array] },
+    Node { data: 'EWR', edgesList: [Array] },
+    Node { data: 'JFK', edgesList: [Array] },
+    Node { data: 'BOS', edgesList: [Array] },
+    Node { data: 'MIA', edgesList: [Array] },
+    Node { data: 'MCO', edgesList: [Array] },
+    Node { data: 'PBI', edgesList: [Array] }
+  ]
+}
+ */
