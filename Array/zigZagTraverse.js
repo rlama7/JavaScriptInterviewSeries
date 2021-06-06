c1a1ece4c351fa4d43904a2038961b2d88d09e00
Ratna Lama <rlama@ratnalamaCOM.local>
2021-06-06 12:00:32 -0700

    added zig zag travese

---
+++ Array/zigZagTraverse.js
@@ -0,0 +1,32 @@
+/**
+ * zigZagTraverse.js    - A program that accepts 2D Array as an input and traverses in a
+ *                        zigzag fashion to return 1D array.
+ * @author              - Ratna Lama
+ * @version             - 1.0.0
+ * @description         - Zig zag order starts at the top left corner of the the 2D array,
+ *                        goes down by one element, and proceeds in a zigzag pattern all the way
+ *                        to the bottom right corner.
+ *
+ * --->INPUT<---
+ *      array = [
+ *                [1,3,4,10],
+ *                [2,5,9,11],
+ *                [6,8,12,15],
+ *                [7,13,14,16]
+ *              ]
+ * --->OUTPUT<---
+ *          [1,2,3,4,5,6,7,8,9,10,11,12,13,,14,15,16]
+ *
+ * --->CONSTRAINTS/RESTRICTIONS<---
+ *
+ * --->EDGE CASES<---
+ *
+ * --->ALGORITHM<---
+ *
+ * --->TIME COMPLEXITY<---
+ * time O() ->
+ * space O() ->
+ * @param {*} array 2D-Array input
+ * @return {*} array 1D-Array
+ */
+const zigZagTraverse = (array) => {};
