c20a7b67da1093d3de0d131b1aebda6f2f7a6a36
Ratna Lama <rlama@ratnalamaCOM.local>
2021-06-06 19:44:26 -0700

    added node and linkedlist class

---
+++ LinkedList/linkedList4.js
@@ -0,0 +1,13 @@
+class Node {
+  // default next to null
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
